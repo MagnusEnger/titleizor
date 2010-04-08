3464b8f093428baed92c6d811ac397f07ec15739
Magnus Enger <magnus@enger.priv.no>
2010-04-08 12:05:33 +0200

    Add licence

--- titleizor.pl
+++ titleizor.pl
@@ -1,5 +1,25 @@
 #!/opt/local/bin/perl -w
 
+# Copyright 2010 Magnus Enger Libriotech
+# 
+# This file is part of titleizor.
+# 
+# titleizor is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# titleizor is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with titleizor. If not, see <http://www.gnu.org/licenses/>.
+# 
+# Source code available from:
+# http://github.com/MagnusEnger/titleizor
+
 use Getopt::Long;
 use Pod::Usage;
 use Image::Magick;
